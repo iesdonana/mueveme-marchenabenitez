b6ec4ed3c59be5c289897327139e9c53032a14ef
Manuel Alejandro Benítez <manuelalejandro.benitez@iesdonana.org>
2019-01-28 17:45:28 +0100

    Base de datos

--- db/mueveme.sql
+++ db/mueveme.sql
@@ -1,4 +1,11 @@
 ------------------------------
 -- Archivo de base de datos --
 ------------------------------
+DROP TABLE IF EXISTS usuarios CASCADE;
 
+CREATE TABLE usuarios
+(
+    id       BIGSERIAL   PRIMARY KEY
+  , nombre   VARCHAR(32) NOT NULL UNIQUE
+  , password VARCHAR(60) NOT NULL
+)
