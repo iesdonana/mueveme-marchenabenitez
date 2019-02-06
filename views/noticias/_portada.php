0a667a5388ee5c3bedf27338edec394b001baddd
Ezequiel Marchena <ezmg92@gmail.com>
2019-02-06 18:51:53 +0100

    Añade link a detalle de noticia y filtra por cat. al clicar en una cat.

--- views/noticias/_portada.php
+++ views/noticias/_portada.php
@@ -10,12 +10,22 @@ use yii\helpers\Html;
         </div>
         <div class="col-xs-11">
             <h4 class="titulo">
-                <?= Html::a($model->titulo, $model->link) ?>
+                <?= Html::a(Html::encode($model->titulo), Html::encode($model->link)) ?>
             </h4>
-            <small><?= 'Enviado por: ' . Html::a($model->usuario->nombre) ?></small>
-            <p> <?= $model->noticia ?> </p>
-            <p> <?= count($model->comentarios) . ' comentarios ' .
-            $model->categoria->categoria ?></p>
+            <small>
+                <?= 'Enviado por: ' .
+                    Html::a(Html::encode($model->usuario->nombre),
+                    ['usuarios/view', 'id' => $model->usuario_id])
+                ?>
+            </small>
+            <p> <?= Html::encode($model->noticia) ?> </p>
+            <p>
+                <?= Html::a(count($model->comentarios) .
+                    ' comentarios ', ['noticias/view', 'id' => $model->id]) .
+                    Html::a(Html::encode($model->categoria->categoria),
+                    ['noticias/index', 'NoticiasSearch[categoria_id]' => $model->categoria_id])
+                ?>
+            </p>
         </div>
     </div>
 </div>
