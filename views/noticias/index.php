7a048bcb9b0eb184680964cdb24309d291115b55
Ezequiel Marchena <ezmg92@gmail.com>
2019-02-06 18:25:14 +0100

    Muestra todas las noticias en portada

--- views/noticias/index.php
+++ views/noticias/index.php
@@ -1,38 +1,19 @@
 <?php
 
-use yii\helpers\Html;
-use yii\grid\GridView;
+use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\NoticiasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Noticias';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="noticias-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Noticias', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
+    <?= ListView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'titulo',
-            'noticia:ntext',
-            'link:url',
-            'usuario.nombre',
-            'categoria.categoria',
-            'created_at:datetime',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
+        'summary' => '',
+        'itemView' => function ($model, $key, $index, $widget) {
+            return $this->render('_portada',['model' => $model]);
+        }
     ]); ?>
 </div>
