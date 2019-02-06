13eaa8f7add57d1c72dc7856885fd4e2e60bbd86
Ezequiel Marchena <ezequiel.marchena@iesdonana.org>
2019-02-06 20:47:01 +0100

    Añade links que faltan y pule detalles

--- views/noticias/_portada.php
+++ views/noticias/_portada.php
@@ -1,26 +1,32 @@
 <?php
 // _list_item.php
 use yii\helpers\Html;
+
+$url = explode("/", $model->link);
+$creado = Yii::$app->formatter->asDateTime($model->created_at, "short")
+
 ?>
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-1">
-            <p> <?= count($model->movimientos) . ' mov.' ?> </p>
+        <div class="col-md-1 text-center">
+            <?= count($model->movimientos) . ' mov.' ?>
+            <button type="button" class="btn btn-success">Muévelo</button>
         </div>
-        <div class="col-xs-11">
+        <div class="col-md-11">
             <h4 class="titulo">
                 <?= Html::a(Html::encode($model->titulo), Html::encode($model->link)) ?>
             </h4>
             <small>
-                <?= 'Enviado por: ' .
+                <?= 'por: ' .
                     Html::a(Html::encode($model->usuario->nombre),
                     ['usuarios/view', 'id' => $model->usuario_id])
+                    . ' a ' . $url[2] . " " . $creado
                 ?>
             </small>
             <p> <?= Html::encode($model->noticia) ?> </p>
             <p>
-                <?= Html::a(count($model->comentarios) .
+                <?= Html::a(Html::encode(count($model->comentarios)) .
                     ' comentarios ', ['noticias/view', 'id' => $model->id]) .
                     Html::a(Html::encode($model->categoria->categoria),
                     ['noticias/index', 'NoticiasSearch[categoria_id]' => $model->categoria_id])
