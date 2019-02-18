90903c379128c99aff68048f5e6779c095a909f4
Manuel Alejandro Benítez <manuelalejandro.benitez@iesdonana.org>
2019-02-18 19:28:30 +0100

    Acercamiento

--- views/comentarios/_comentario.php
+++ views/comentarios/_comentario.php
@@ -45,6 +45,48 @@ $creado = Yii::$app->formatter->asDateTime($model->created_at, "short")
                         </button>
                         <?= Html::endForm() ?>
                     </div>
+                    <div class="col">
+                        <a class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal" title="Responder">
+                            <span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Responder Comentario</h4>
+                      </div>
+                      <div class="modal-body">
+                          <?php
+                          $com1 = new Comentarios();
+                          $com1->usuario_id = Yii::$app->user->id;
+                          $com1->created_at = new Expression('NOW()');
+                          $com1->noticia_id = $model->noticia_id;
+                          $com1->comentario_id = $model->id;
+                          $form1 = ActiveForm::begin([
+                              'method' => 'POST',
+                              'action' => Url::to(['comentarios/responder']),
+                          ]); ?>
+
+                          <?= $form1->field($com1, 'comentario')->textarea(['rows' => 2])->label('Respuesta') ?>
+
+                          <?= $form1->field($com1, 'usuario_id')->textInput()->hiddenInput()->label(false) ?>
+
+                          <?= $form1->field($com1, 'noticia_id')->textInput()->hiddenInput()->label(false) ?>
+
+                          <?= $form1->field($com1, 'comentario_id')->textInput()->hiddenInput()->label(false) ?>
+
+                          <?= $form1->field($com1, 'created_at')->textInput()->hiddenInput()->label(false) ?>
+                      </div>
+                      <div class="modal-footer">
+                             <button class="btn btn-xs btn-success" type="submit" name="button">Responder</button>
+                          <?php ActiveForm::end(); ?>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div class="col-md-offset-1">
                     <?php
